19e72e1d64c5aad17d6865b4d1488584a04a71c0
hoang.nguyen13 <hoang.nguyen13@mservice.com.vn>
2023-01-09 09:04:27 +0700

    feat: init lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import Counter from "./features/counter/Counter";
 
 function App() {
-	return (
-		<main className="App">
-			<Counter />
-		</main>
-	);
+	return <main className="App"></main>;
 }
 
 export default App;
